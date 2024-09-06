6befab2c65317ddf3de005393af5e307279beaa0
ffers <fferses@gmail.com>
2024-09-06 11:17:34 +0200

    edit push update status in message

--- a_service/tg_serv.py
+++ a_service/tg_serv.py
@@ -85,6 +85,7 @@ class TgServ():
         data_get_order = (
             f"{product_article} Сумма: {order.sum_price} \n\n{description}\n\n"
             f"{order.city_name}, {order.warehouse_text} \n\n🍏 {order.source_order.name} Замовлення № {order.order_id_sources}\n"
+            f"Cтатус: {order.ordered_status.name}\n"
             f"\n{order.phone};{ttn}\n{order.client_lastname} {order.client_firstname}\n"
             f"Спосіб доставки - {order.delivery_method.name}\n"
             f"Спосіб оплати - {payment_method.name}, {sum_check} \n\n"
