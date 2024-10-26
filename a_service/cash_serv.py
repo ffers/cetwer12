2366def2a9a079a7d7bbcf3dba9f4997b85c8782
ffers <fferses@gmail.com>
2024-10-26 23:10:03 +0200

    add if item.money

--- a_service/cash_serv.py
+++ a_service/cash_serv.py
@@ -53,7 +53,7 @@ class CacheService:
         stock = 0
         if items:
             for item in items:
-                if hasattr(item, 'money'):
+                if isinstance(item, object) and hasattr(item, 'money'):
                     stock += item.money
         self.set("stock", stock)
         return stock
