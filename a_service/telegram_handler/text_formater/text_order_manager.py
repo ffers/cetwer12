6abd23a65a1726d62b800f9e57846753092c5d04
ffers <fferses@gmail.com>
2025-04-01 17:38:37 +0200

    update 10425

--- a_service/telegram_handler/text_formater/text_order_manager.py
+++ a_service/telegram_handler/text_formater/text_order_manager.py
@@ -55,7 +55,7 @@ class TextOrderManager:
         self.tx_bl.order_number_description = self.order_number_description(self.order)
         self.tx_bl.costumer_comment = self.costumer_comment(self.order)
         self.tx_bl.product_block_header = self.product_block_header(self.order)
-        self.tx_bl.costumer_name = self.recipient_name(self.order)
+        self.tx_bl.costumer_name = self.costumer_name(self.order)
         self.tx_bl.costumer_phone = self.contact_number_phone_number(self.order)
         self.tx_bl.address_line = self.address_line(self.order)
         self.tx_bl.payment_method_sum = self.payment_method_sum(self.order)
@@ -68,17 +68,18 @@ class TextOrderManager:
     def general_text(self):
         return (
             f"{self.tx_bl.status_order}"
-            f"{self.tx_bl.order_number_description}"
-            f"{self.tx_bl.costumer_comment}"
-            f"{self.tx_bl.product_block_header}"
+            f"{self.tx_bl.order_number_description}\n"
+            f"{self.tx_bl.product_block_header}\n"
+            f"{self.tx_bl.costumer_comment}\n"
             f"{self.tx_bl.costumer_name}"
-            f"{self.tx_bl.costumer_phone}"
+            f"{self.tx_bl.costumer_phone}\n"
             f"{self.tx_bl.address_line}"
             f"{self.tx_bl.payment_method_sum}"
             f"{self.tx_bl.delivery_method}"
             f"{self.tx_bl.recipient_name}"
-            f"{self.tx_bl.recipient_phone}"
+            f"{self.tx_bl.recipient_phone}\n"
             f"{self.tx_bl.product_footer}"
+            f"🫴"
         )
     
     def status_order(self, order: OrderDTO):
@@ -94,12 +95,12 @@ class TextOrderManager:
     
     def costumer_comment(self, order):
         if order.description:
-            return f"\n{self.store_color} Нотатка:\n" + order.description
+            return f"{self.store_color} Нотатка:\n" + order.description
         else:
-            return "\nНотаток від клієнта нема\n"
+            return "Нотаток від клієнта нема\n"
         
     def costumer_name(self, order):
-        text = "\nПокупець:\n"
+        text = "Покупець:\n"
         text += f"{order.costumer.last_name} {order.costumer.first_name}\n"
         return text
     
@@ -120,7 +121,7 @@ class TextOrderManager:
             return order.sum_price 
     
     def recipient_name(self, order):
-        text = "\nОтримувач:\n"
+        text = "Отримувач:\n"
         text += f"{order.recipient.last_name} {order.recipient.first_name}\n"
         return text
 
@@ -130,17 +131,17 @@ class TextOrderManager:
     
     
     def order_number_description(self, order):
-        text = f"\n{self.store_color} {order.source_order.name} "
+        text = f"{self.store_color} {order.source_order.name} "
         text += f"Замовлення № {order.order_code}\n"
         return text
     
     def contact_number_phone_number(self, order):
         ttn = order.ttn if order.ttn else "ТТН немає"        
-        return f"\n{order.costumer.phone};{ttn}\n"
+        return f"{order.costumer.phone};{ttn}\n"
     
     def address_line(self, order):
         city_name = f"{order.city_name}, " if order.city_name else ""
-        return f"\n{city_name}{order.warehouse_text}\n"
+        return f"{city_name}{order.warehouse_text}\n"
     
     def delivery_method(self, order: OrderDTO):
         return f"{order.delivery_method.name}"
